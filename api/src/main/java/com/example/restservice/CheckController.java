8aca7ed70d215b5f11365a83598006eef2f9c82c
Joed Cueto <cueto.joed@gmail.com>
2021-06-14 17:45:25 +1000

    Update code

--- api/src/main/java/com/example/restservice/CheckController.java
+++ api/src/main/java/com/example/restservice/CheckController.java
@@ -1,5 +1,7 @@
 package com.example.restservice;
 
+import com.example.vo.BtsConstants;
+import com.example.vo.BtsResponse;
 import com.example.vo.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +15,28 @@ public class CheckController {
         Person person = new Person();
         person.setFirstName("Joed");
         person.setLastName("Cueto");
-        if ("Stefanie".equalsIgnoreCase(name)) {
-            person.setFirstName("Stefanie");
-            person.setLastName("Jara");
+        if ("".equalsIgnoreCase(name)) {
+            person.setFirstName("");
+            person.setLastName("");
         }
         return person;
+    }
+
+    @GetMapping("/bts")
+    public BtsResponse getBTS(@RequestParam(value = "name", defaultValue = "") String name) {
+        BtsResponse response = new BtsResponse();
+        boolean isBts = false;
+
+        for (String bts : BtsConstants.BTS_NAMES) {
+            if (bts.equalsIgnoreCase(name)) {
+               isBts = true;
+            }
+        }
+        if (isBts) {
+            response.setMessage("You're an armmmyyyyy. Yay!!!");
+        } else {
+            response.setMessage("EPAL ka, hindi yan BTS!");
+        }
+        return response;
     }
 }
---
+++ api/src/main/java/com/example/vo/BtsConstants.java
@@ -0,0 +1,10 @@
+package com.example.vo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BtsConstants {
+
+    public static final List<String> BTS_NAMES = Arrays.asList("Namjoon", "Seokjin", "Yoongi", "Hoseok", "Jimin", "Taehyung", "Jungkook");
+
+}
